c1b1193b35e8c8c10b76f6159d54e4706bad598d
chaukentsakiso55-a11y <chaukentsakiso55@gmail.com>
2026-09-05 19:05:36 +0200

    Add Level 3 Palace Under Siege director

---
+++ Source/TheForbiddenGates/Public/TFGLevelThreeDirector.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TFGLevelThreeDirector.generated.h"
+
+class ATFGCharacterBase;
+class UTFGObjectiveWidget;
+class UTFGQuestComponent;
+
+UCLASS()
+class THEFORBIDDENGATES_API ATFGLevelThreeDirector : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ATFGLevelThreeDirector();
+
+protected:
+    virtual void BeginPlay() override;
+
+private:
+    UFUNCTION()
+    void HandleQuestChanged(FName QuestId, int32 Stage);
+
+    void InitializeMission();
+    void ApplyObjectiveForStage(int32 Stage);
+    void FinishMission();
+
+    FName QuestId = TEXT("L03_PalaceUnderSiege");
+    TWeakObjectPtr<ATFGCharacterBase> PlayerCharacter;
+    TWeakObjectPtr<UTFGQuestComponent> QuestComponent;
+    UPROPERTY() TObjectPtr<UTFGObjectiveWidget> ObjectiveWidget;
+    bool bFinished = false;
+};
