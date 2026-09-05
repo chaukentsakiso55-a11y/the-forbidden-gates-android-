3fdb151b276458312a04f54c004c61abe06874a9
chaukentsakiso55-a11y <chaukentsakiso55@gmail.com>
2026-09-05 18:31:00 +0200

    Add Android combat control widget

---
+++ Source/TheForbiddenGates/Public/TFGMobileControlsWidget.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TFGMobileControlsWidget.generated.h"
+
+class ATFGPlayerCharacter;
+
+UCLASS()
+class THEFORBIDDENGATES_API UTFGMobileControlsWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+public:
+    void SetPlayerCharacter(ATFGPlayerCharacter* InPlayerCharacter);
+
+protected:
+    virtual void NativeConstruct() override;
+
+private:
+    UFUNCTION() void HandleMagicPressed();
+    UFUNCTION() void HandleInteractPressed();
+    UFUNCTION() void HandleJumpPressed();
+
+    TWeakObjectPtr<ATFGPlayerCharacter> PlayerCharacter;
+};
