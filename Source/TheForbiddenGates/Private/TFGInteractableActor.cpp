98dbd839942d11d06edc0720a4aef1661d31e6fc
chaukentsakiso55-a11y <chaukentsakiso55@gmail.com>
2026-09-05 18:34:32 +0200

    Add visible prompts to interactable mission markers

--- Source/TheForbiddenGates/Private/TFGInteractableActor.cpp
+++ Source/TheForbiddenGates/Private/TFGInteractableActor.cpp
@@ -1,13 +1,37 @@
 #include "TFGInteractableActor.h"
 #include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/TextRenderComponent.h"
+#include "Engine/StaticMesh.h"
+#include "UObject/ConstructorHelpers.h"
 #include "TFGCharacterBase.h"
 #include "TFGQuestComponent.h"
 
 ATFGInteractableActor::ATFGInteractableActor()
 {
     PrimaryActorTick.bCanEverTick = false;
+
     SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
     SetRootComponent(SceneRoot);
+
+    MarkerMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MarkerMesh"));
+    MarkerMesh->SetupAttachment(SceneRoot);
+    MarkerMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+    MarkerMesh->SetCollisionResponseToAllChannels(ECR_Block);
+    MarkerMesh->SetRelativeScale3D(FVector(0.55f));
+
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereMesh(TEXT("/Engine/BasicShapes/Sphere.Sphere"));
+    if (SphereMesh.Succeeded())
+    {
+        MarkerMesh->SetStaticMesh(SphereMesh.Object);
+    }
+
+    PromptLabel = CreateDefaultSubobject<UTextRenderComponent>(TEXT("PromptLabel"));
+    PromptLabel->SetupAttachment(SceneRoot);
+    PromptLabel->SetRelativeLocation(FVector(0.0f, 0.0f, 125.0f));
+    PromptLabel->SetHorizontalAlignment(EHTA_Center);
+    PromptLabel->SetWorldSize(24.0f);
+    PromptLabel->SetText(FText::FromString(TEXT("Interact")));
 }
 
 FText ATFGInteractableActor::GetInteractionPrompt_Implementation(APawn* Interactor) const
@@ -50,7 +74,18 @@ void ATFGInteractableActor::Interact_Implementation(APawn* Interactor)
     if (bOneShot)
     {
         bConsumed = true;
+        if (MarkerMesh) MarkerMesh->SetVisibility(false, true);
+        if (PromptLabel) PromptLabel->SetVisibility(false, true);
+        SetActorEnableCollision(false);
     }
 
     OnInteracted.Broadcast(InteractionId, Interactor);
+}
+
+void ATFGInteractableActor::RefreshPresentation()
+{
+    if (PromptLabel)
+    {
+        PromptLabel->SetText(InteractionPrompt);
+    }
 }
